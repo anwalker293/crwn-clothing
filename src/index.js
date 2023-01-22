2bc0f74b13413e880e950aaa2326f257e1690634
Alex Walker <alex.walker@indicio.tech>
2023-01-22 10:46:25 +0100

    feat: redux selectors
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
 
 import App from "./App";
-import { UserProvider } from "./contexts/user.context";
-import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { store } from "./store/store";
 
 import "./index.scss";
 
@@ -17,11 +15,9 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
