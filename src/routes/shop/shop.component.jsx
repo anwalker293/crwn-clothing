e2f320c96c39e48a1e4eb00a31e955b63ea892ad
Alex Walker <alex.walker@indicio.tech>
2023-01-28 16:53:55 +0100

    feat: scratch
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -15,8 +15,10 @@ const Shop = () => {
 
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments("categories");
+      console.log(categoriesArray);
+
+      // dispatch(setCategoriesMap(categoryMap));
     };
     getCategoriesMap();
   }, []);
