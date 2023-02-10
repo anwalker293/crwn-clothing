8fcd981ea76a5d7045eeaf03dd7636158689fcee
Alex Walker <alex.walker@indicio.tech>
2023-02-10 12:43:17 +0100

    feat: set up stripe card, initial
    Signed-off-by: Alex Walker <alex.walker@indicio.tech>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
+import { stripePromise } from "../src/utils/stripe/stripe.utils.js";
 
 import "./index.scss";
 
@@ -17,7 +19,9 @@ render(
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-            <App />
+            <Elements stripe={stripePromise}>
+              <App />
+            </Elements>
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>
